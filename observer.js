c0e3c2b9a9c555528b28a737f1366c3d64051d01
Simon Lee <simonhlee97@hotmail.com>
2023-02-09 12:41:40 +0900

    scroll animations

---
+++ observer.js
@@ -0,0 +1,19 @@
+const allSections = document.querySelectorAll('.section')
+
+const revealSection = function (entries, observer) {
+    const [entry] = entries
+
+    if (!entry.isIntersecting) return
+    entry.target.classList.remove('section--hidden')
+    observer.unobserve(entry.target)
+}
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+    root: null,
+    threshold: [0, 0.1]
+})
+
+allSections.forEach(function (section) {
+    sectionObserver.observe(section)
+    section.classList.add('section--hidden')
+})

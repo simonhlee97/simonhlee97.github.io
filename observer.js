3bbe550586ebfc3794c1903f161fd9e6c7125934
Simon Lee <simonhlee97@hotmail.com>
2023-02-21 18:14:19 +0900

    changed font to courier-new

--- observer.js
+++ observer.js
@@ -1,19 +1,45 @@
-const allSections = document.querySelectorAll('.section')
+// const allSections = document.querySelectorAll('.section')
 
-const revealSection = function (entries, observer) {
-    const [entry] = entries
+// const revealSection = function (entries, observer) {
+//     const [entry] = entries
 
-    if (!entry.isIntersecting) return
-    entry.target.classList.remove('section--hidden')
-    observer.unobserve(entry.target)
-}
+//     if (!entry.isIntersecting) return
+//     entry.target.classList.remove('section--hidden')
+//     observer.unobserve(entry.target)
+// }
 
-const sectionObserver = new IntersectionObserver(revealSection, {
-    root: null,
-    threshold: [0, 0.1]
+// const sectionObserver = new IntersectionObserver(revealSection, {
+//     root: null,
+//     threshold: [0, 0.1]
+// })
+
+// allSections.forEach(function (section) {
+//     sectionObserver.observe(section)
+//     section.classList.add('section--hidden')
+// })
+
+// square animate tutorial
+// square animate tutorial
+// square animate tutorial
+// square animate tutorial
+// square animate tutorial
+
+// Remove the transition class
+const section = document.querySelectorAll('.section');
+section.forEach((sect) => { 
+  sect.classList.remove('section-transition');
 })
 
-allSections.forEach(function (section) {
-    sectionObserver.observe(section)
-    section.classList.add('section--hidden')
-})
+// Create the observer, same as before:
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      section.classList.add('section-transition');
+      return;
+    }
+
+    sections.classList.remove('section-transition');
+  });
+});
+
+observer.observe(document.querySelectorAll('.section-wrapper'));

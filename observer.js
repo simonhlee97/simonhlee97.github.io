5e7ef7b7c794fd1326b9fd322060bc3c96654240
Simon Lee <simonhlee97@hotmail.com>
2024-02-03 09:06:50 -0500

    new homepage 2024

--- observer.js
+++ observer.js
@@ -1,45 +1,19 @@
-// const allSections = document.querySelectorAll('.section')
-
-// const revealSection = function (entries, observer) {
-//     const [entry] = entries
-
-//     if (!entry.isIntersecting) return
-//     entry.target.classList.remove('section--hidden')
-//     observer.unobserve(entry.target)
-// }
-
-// const sectionObserver = new IntersectionObserver(revealSection, {
-//     root: null,
-//     threshold: [0, 0.1]
-// })
-
-// allSections.forEach(function (section) {
-//     sectionObserver.observe(section)
-//     section.classList.add('section--hidden')
-// })
-
-// square animate tutorial
-// square animate tutorial
-// square animate tutorial
-// square animate tutorial
-// square animate tutorial
-
 // Remove the transition class
-const section = document.querySelectorAll('.section');
-section.forEach((sect) => { 
-  sect.classList.remove('section-transition');
+const section = document.querySelectorAll('.section')
+section.forEach((sect) => {
+  sect.classList.remove('section-transition')
 })
 
 // Create the observer, same as before:
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      section.classList.add('section-transition');
-      return;
+      section.classList.add('section-transition')
+      return
     }
 
-    sections.classList.remove('section-transition');
-  });
-});
+    sections.classList.remove('section-transition')
+  })
+})
 
-observer.observe(document.querySelectorAll('.section-wrapper'));
+observer.observe(document.querySelectorAll('.section-wrapper'))
